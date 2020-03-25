a934b730295ee6cc2dbcad0c48bb1ff9d5d08740
Jonathan <IgnusG@users.noreply.github.com>
2020-03-25 16:09:03 +0100

    Add option to exclude individual default terms via new option
    One can use this option to cherry-pick which terms they would like to
    exclude and they can combine this with extra terms to provide their own:
    
    ```
      "defaultTerms": true,
      "excludeDefaults": [ "Airbnb" ],
      "terms": [ "AiRbNb" ]
    ```

--- index.js
+++ index.js
@@ -90,12 +90,13 @@ function getTerms(defaultTerms, terms, exclude) {
 		? loadJson(path.resolve(__dirname, 'terms.json'))
 		: [];
 	const extras = typeof terms === 'string' ? loadJson(terms) : terms;
-	const listTerms = defaults.concat(extras);
 
+	const listTerms = [...(Array.isArray(extras) ? extras : []), ...defaults];
+
+	// Filter on all terms
 	if (Array.isArray(exclude)) {
-		return listTerms.filter(term => exclude.indexOf(term) === -1);
+		return listTerms.filter(term => !exclude.includes(term));
 	}
-
 	return listTerms;
 }
 
